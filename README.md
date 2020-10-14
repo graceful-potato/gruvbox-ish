68d4e5adb7c47261df9cf76a82b1004a2818f80a
Graceful Potato <gracefulpotatow@gmail.com>
2020-10-14 18:19:53 +0300

    Update hs test file. Change colors for json and add json file to the test. Change screenshots

--- README.md
+++ README.md
@@ -3,40 +3,40 @@ It uses modified grammar file for ruby which can distinguish `end` keyword betwe
 Supports ruby, c, css, scss, sass, html, js, ts, sh, py.
 
 ## Hard
-![hard](https://user-images.githubusercontent.com/56965223/71326892-0e6ec180-2512-11ea-9023-f9a38e7c4799.png)
+![hard](https://user-images.githubusercontent.com/56965223/96015763-9bdc2300-0e50-11eb-87b2-ee709350cc4c.png)
 
 ## Medium
-![medium](https://user-images.githubusercontent.com/56965223/71326893-1169b200-2512-11ea-89d7-c80b53f33515.png)
+![medium](https://user-images.githubusercontent.com/56965223/96015778-a0084080-0e50-11eb-9dfa-ac3aecd78c20.png)
 
 ## Soft
-![soft](https://user-images.githubusercontent.com/56965223/71326896-13cc0c00-2512-11ea-98a6-1ac165aafffb.png)
+![soft](https://user-images.githubusercontent.com/56965223/96015789-a4ccf480-0e50-11eb-86aa-558176c0f3da.png)
 
 ### Ruby
-![rb](https://user-images.githubusercontent.com/56965223/71312307-486c9480-243a-11ea-8fce-b9b98e6120b7.png)
-
-### C
-![c](https://user-images.githubusercontent.com/56965223/71312310-54585680-243a-11ea-8da2-b29c9ab154fe.png)
-
-### CSS
-![css](https://user-images.githubusercontent.com/56965223/71312311-58847400-243a-11ea-9a3c-74a30f609b27.png)
-
-### SCSS
-![scss](https://user-images.githubusercontent.com/56965223/71312357-d9dc0680-243a-11ea-9a51-99dacb4786f1.png)
-
-### SASS
-![sass](https://user-images.githubusercontent.com/56965223/71312360-dc3e6080-243a-11ea-896d-c3980a20bb05.png)
-
-### HTML
-![html](https://user-images.githubusercontent.com/56965223/71312322-6f2acb00-243a-11ea-9e94-25480c2086cc.png)
-
-### TypeScript
-![ts](https://user-images.githubusercontent.com/56965223/71312325-7225bb80-243a-11ea-9380-800b03ae88d7.png)
+![rb](https://user-images.githubusercontent.com/56965223/96015847-b2827a00-0e50-11eb-8946-d3852f152c03.png)
 
 ### JavaScript
-![js](https://user-images.githubusercontent.com/56965223/71312327-77830600-243a-11ea-914b-cd622a5a332e.png)
+![js](https://user-images.githubusercontent.com/56965223/96016225-2b81d180-0e51-11eb-8a12-a5e5e843ca27.png)
+
+### TypeScript
+![ts](https://user-images.githubusercontent.com/56965223/96016235-2de42b80-0e51-11eb-99ef-d3869f070774.png)
 
 ### Python
-![py](https://user-images.githubusercontent.com/56965223/73128281-df021780-3fdd-11ea-9d39-ac38772d1140.png)
+![py](https://user-images.githubusercontent.com/56965223/96016310-45231900-0e51-11eb-8226-9d4218eb80f5.png)
 
-### Shell
-![sh](https://user-images.githubusercontent.com/56965223/71312364-e3fe0500-243a-11ea-8a54-b1cff03cc9a9.png)
+### Haskell
+![hs](https://user-images.githubusercontent.com/56965223/96016330-494f3680-0e51-11eb-933f-61f9f2f796cf.png)
+
+### HTML
+![html](https://user-images.githubusercontent.com/56965223/96016159-1ad15b80-0e51-11eb-9a03-7ac61469c057.png)
+
+### CSS
+![css](https://user-images.githubusercontent.com/56965223/96016174-20c73c80-0e51-11eb-8c7e-71eada5fcf4c.png)
+
+### SCSS
+![scss](https://user-images.githubusercontent.com/56965223/96016186-22910000-0e51-11eb-9314-6fe00e50b3da.png)
+
+### SASS
+![sass](https://user-images.githubusercontent.com/56965223/96016193-23c22d00-0e51-11eb-91c5-f842ec9e9893.png)
+
+### JSON
+![json](https://user-images.githubusercontent.com/56965223/96016265-363c6680-0e51-11eb-8ceb-631f0ab0122a.png)
--- tests/.hs
+++ tests/.hs
@@ -1,413 +1,64 @@
--- Copyright 2016 TensorFlow authors.
---
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
---
---     http://www.apache.org/licenses/LICENSE-2.0
---
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
+data Point = Point Float Float deriving (Show)
+data Shape 
+  = Circle Point Float 
+  | Rectangle Point Point
+  deriving (Show)
 
--- | This module contains definitions for some built-in TensorFlow operations.
---
--- Note that certain, "stateful" ops like 'variable' and 'assign' return a
--- 'Build' action (e.g., @Build (Tensor Ref a)@ instead of a pure value; the
--- returned 'Tensor's are always rendered in the current 'Build' context.  This
--- approach helps us avoid problems with inlining or common subexpression
--- elimination, by writing
---
--- > do
--- >     v <- variable []
--- >     w <- assign v 3
--- >     render $ w * w
---
--- instead of
---
--- > let
--- >    v = variable []
--- >    w = assign v 3
--- > in w * w
---
--- since the latter could be reasonably transformed by the compiler into (or
--- vice versa)
---
--- > let
--- >    v = variable []
--- >    w = assign v 3
--- >    w' = assign v 3
--- > in w * w'
---
--- Ops should return a 'Build' action if their original 'OpDef' marks them as
--- stateful, or if they take any Refs as input.  (This mirrors the rules that
--- TensorFlow uses to avoid common subexpression elimination.)
-{-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE OverloadedLists #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE UndecidableInstances #-}
-{-# OPTIONS_GHC -fno-warn-orphans #-}
+-- instance Show Shape where
+--   show (Circle x y r) = printf "Circle %f %f %f" x y r
+--   show (Rectangle x1 y1 x2 y2) = printf "Rectangle %f %f %f %f" x1 y1 x2 y2
 
-module TensorFlow.Ops
-    ( CoreOps.add
-    , CoreOps.add'
-    , CoreOps.abs
-    , CoreOps.abs'
-    , CoreOps.addN
-    , CoreOps.addN'
-    , CoreOps.argMax
-    , CoreOps.argMax'
-    , CoreOps.assign
-    , CoreOps.assign'
-    , CoreOps.broadcastGradientArgs
-    , CoreOps.broadcastGradientArgs'
-    , CoreOps.cast
-    , CoreOps.cast'
-    , CoreOps.concat
-    , CoreOps.concat'
-    , constant
-    , constant'
-    , CoreOps.equal
-    , CoreOps.equal'
-    , expandDims
-    , expandDims'
-    , initializedVariable
-    , initializedVariable'
-    , zeroInitializedVariable
-    , zeroInitializedVariable'
-    , CoreOps.fill
-    , CoreOps.fill'
-    , CoreOps.identity
-    , CoreOps.identity'
-    , CoreOps.matMul
-    , CoreOps.matMul'
-    , matTranspose
-    , matTranspose'
-    , CoreOps.mean
-    , CoreOps.mean'
-    , CoreOps.mul
-    , CoreOps.mul'
-    , CoreOps.neg
-    , CoreOps.neg'
-    , CoreOps.oneHot
-    , CoreOps.oneHot'
-    , CoreOps.pack
-    , CoreOps.pack'
-    , placeholder
-    , placeholder'
-    , CoreOps.range
-    , CoreOps.range'
-    , reducedShape
-    , reduceMean
-    , reduceMean'
-    , CoreOps.relu
-    , CoreOps.relu'
-    , CoreOps.reluGrad
-    , CoreOps.reluGrad'
-    , CoreOps.tanh
-    , CoreOps.tanhGrad
-    , CoreOps.reshape
-    , CoreOps.reshape'
-    , restore
-    , restoreFromName
-    , save
-    , scalar
-    , scalar'
-    , shape
-    , shape'
-    , CoreOps.sigmoid
-    , CoreOps.sigmoidGrad
-    , CoreOps.sign
-    , CoreOps.sign'
-    , CoreOps.size
-    , CoreOps.size'
-    , CoreOps.softmax
-    , CoreOps.softmax'
-    , CoreOps.softmaxCrossEntropyWithLogits
-    , CoreOps.softmaxCrossEntropyWithLogits'
-    , CoreOps.sparseToDense
-    , CoreOps.sparseToDense'
-    , CoreOps.sub
-    , CoreOps.sub'
-    , CoreOps.sum
-    , CoreOps.sum'
-    , reduceSum
-    , reduceSum'
-    , CoreOps.transpose
-    , CoreOps.transpose'
-    , truncatedNormal
-    , truncatedNormal'
-    , CoreOps.variable
-    , CoreOps.variable'
-    , vector
-    , vector'
-    , zeros
-    , CoreOps.zerosLike
-    , CoreOps.zerosLike'
-    , scalarize
-    ) where
+area :: Shape -> Float
+area (Circle _ r) = pi * r ^ 2
+area (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
 
-import Data.ByteString (ByteString)
-import Data.Complex (Complex)
-import Data.Int (Int32, Int64)
-import Data.Word (Word16)
-import Prelude hiding (abs, sum, concat)
-import Data.ProtoLens.Default(def)
-import Data.Text.Encoding (encodeUtf8)
-import Lens.Family2 ((.~), (&))
-import Text.Printf (printf)
-import Proto.Tensorflow.Core.Framework.Tensor  (TensorProto)
-import Proto.Tensorflow.Core.Framework.Tensor_Fields
-    ( dtype
-    , tensorShape
-    )
-import qualified Proto.Tensorflow.Core.Framework.TensorShape_Fields
-  as TensorShape
+nudge :: Shape -> Float -> Float -> Shape
+nudge (Circle (Point x y) r) a b = Circle (Point (x + a) (y + b)) r
+nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b = Rectangle (Point (x1 + a) (y1 + b)) (Point (x2 + a) (y2 + b))
 
-import TensorFlow.Build
-import TensorFlow.BuildOp
-import TensorFlow.ControlFlow (group)
-import TensorFlow.Tensor
-import TensorFlow.Types
+baseCircle :: Float -> Shape
+baseCircle r = Circle (Point 0 0) r
 
-import qualified TensorFlow.GenOps.Core as CoreOps
+baseRect :: Float -> Float -> Shape
+baseRect width height = Rectangle (Point 0 0) (Point width height)
 
-import qualified Prelude (abs)
+data Person = Person { firstName :: String
+                     , lastName :: String
+                     , age :: Int
+                     , height :: Float
+                     , phoneNumber :: String
+                     , flavor :: String } deriving (Show)
 
--- TODO: Look into hs-boot refactoring to allow mutually recursive imports.
--- | Must be defined as an orphan because of the dependency order between Ops
--- and Tensor.
---
--- The indirect constraint "v ~ Value" helps disambiguate types, for example in
--- "neg 1 :: Tensor Value Float", it helps find the type of the subexpression
--- "1".
-instance ( TensorType a
-         , Num a
-         , v ~ Build
-         , OneOf '[ Double, Float, Int32, Int64
-                  , Complex Float, Complex Double] a) => Num (Tensor v a) where
-    (+) = CoreOps.add
-    (*) = CoreOps.mul
-    (-) = CoreOps.sub
-    abs = CoreOps.abs
-    fromInteger = scalar . fromInteger
-    signum = CoreOps.sign
-    negate = CoreOps.neg
+data Vector a = Vector a a a deriving (Show)
 
-matTranspose :: TensorType a => Tensor e a -> Tensor Build a
-matTranspose = matTranspose' id
+vplus :: (Num a) => Vector a -> Vector a -> Vector a
+(Vector a b c) `vplus` (Vector x y z) = Vector (a + x) (b + y) (c + z)
 
-matTranspose' :: TensorType a => OpParams -> Tensor v a -> Tensor Build a
-matTranspose' params = flip (CoreOps.transpose' params) (vector [1, 0 :: Int32])
+scalarProd :: (Num a) => Vector a -> Vector a -> a
+(Vector a b c) `scalarProd` (Vector x y z) = (a * x) + (b * y) + (c * z)
 
-placeholder :: (MonadBuild m, TensorType a) => Shape -> m (Tensor Value a)
-placeholder = placeholder' id
+vmult :: (Num a) => Vector a -> Vector a -> Vector a
+(Vector a b c) `vmult` (Vector x y z) = Vector (a * x) (b * y) (c * z)
 
-placeholder' :: forall m a . (MonadBuild m, TensorType a)
-             => OpParams -> Shape -> m (Tensor Value a)
-placeholder' params pShape
-    -- Note: we don't use CoreOps.placeholder' since that op isn't stateful,
-    -- and thus would be CSE'd.
-    = build $ buildOp [] $ opDef "Placeholder"
-                & opAttr "dtype" .~ tensorType (undefined :: a)
-                & opAttr "shape" .~ pShape
-                & params
+main = do
+  let rectangle = Rectangle (Point 0 0) (Point 100 100)
+  let circle = Circle (Point 0 0) 24
+  let person = Person { firstName="John"
+                      , lastName="Smith"
+                      , age=25
+                      , height=1.84
+                      , phoneNumber="+12345678"
+                      , flavor="chocolate"}
 
--- | Creates a variable initialized to the given value.
--- Initialization happens next time session runs.
-initializedVariable :: (MonadBuild m, TensorType a)
-                    => Tensor v a -> m (Tensor Ref a)
-initializedVariable = initializedVariable' id
+  print $ area rectangle
+  print $ area circle
 
-initializedVariable' :: (MonadBuild m, TensorType a)
-                    => OpParams -> Tensor v a -> m (Tensor Ref a)
-initializedVariable' params initializer = do
-    v <- CoreOps.variable' params []  -- The shape is not known initially.
-    i <- CoreOps.assign' (opAttr "validate_shape" .~ False) v
-                            initializer
-    addInitializer =<< group i
-    return v
+  print $ nudge circle 2.5 1.2
+  print $ nudge rectangle 10 20
 
--- | Creates a zero-initialized variable with the given shape.
-zeroInitializedVariable
-  :: (MonadBuild m, TensorType a, Num a) =>
-     TensorFlow.Types.Shape -> m (Tensor TensorFlow.Tensor.Ref a)
-zeroInitializedVariable = zeroInitializedVariable' id
+  print $ baseCircle 5
+  print $ baseRect 15 5
 
-zeroInitializedVariable'
-  :: (MonadBuild m, TensorType a, Num a) =>
-     OpParams -> TensorFlow.Types.Shape -> m (Tensor TensorFlow.Tensor.Ref a)
-zeroInitializedVariable' params = initializedVariable' params . zeros
+  print $ nudge (baseRect 40 100) 60 23
 
--- TODO: Support heterogeneous list of tensors.
-save :: forall a m v . (Rendered (Tensor v), MonadBuild m, TensorType a)
-        => ByteString    -- ^ File path.
-        -> [Tensor v a]  -- ^ Tensors to save.
-        -> m ControlNode
-save path xs = build $ do
-    let toByteStringTensor = scalar . encodeUtf8 . encodeOutput . renderedOutput
-    let names = fmap toByteStringTensor xs
-    let types = replicate (length xs) (tensorType (undefined :: a))
-    names' <- buildInputs $ CoreOps.pack names
-    xs' <- buildInputs xs
-    path' <- buildInputs $ scalar path
-    buildOp [] $ opDef "Save"
-                    & opAttr "T" .~ types
-                    & opInputs .~ (path' ++ names' ++ xs')
-
--- | Restore a tensor's value from a checkpoint file.
---
--- This version allows restoring from a checkpoint file that uses a different
--- tensor name than the variable.
-restoreFromName :: forall a m . (MonadBuild m, TensorType a)
-                => ByteString    -- ^ File path.
-                -> ByteString    -- ^ Tensor name override.
-                -> Tensor Ref a  -- ^ Tensor to restore.
-                -> m ControlNode
-restoreFromName path name x = build $ do
-    path' <- buildInputs $ scalar path
-    name' <- buildInputs $ scalar name
-    restoreOp <- buildOp [] $ opDef "Restore"
-                               & opAttr "dt" .~ tensorType (undefined :: a)
-                               & opInputs .~ (path' ++ name')
-    group =<< CoreOps.assign x (restoreOp :: Tensor Value a)
-
--- | Restore a tensor's value from a checkpoint file.
-restore :: forall a m . (MonadBuild m, TensorType a)
-        => ByteString    -- ^ File path.
-        -> Tensor Ref a  -- ^ Tensor to restore.
-        -> m ControlNode
-restore path x = restoreFromName path name x
-  where
-    name = encodeUtf8 $ encodeOutput $ renderedOutput x
-
--- | Create a constant tensor.
---
--- The values should be in row major order, e.g.,
---
---   element 0:   index (0, ..., 0)
---   element 1:   index (0, ..., 1)
---   ...
-constant :: TensorType a => Shape -> [a] -> Tensor Build a
-constant = constant' id
-
-constant' :: forall a . TensorType a => OpParams -> Shape -> [a] -> Tensor Build a
-constant' params (Shape cShape) values
-    | invalidLength = error invalidLengthMsg
-    | otherwise = CoreOps.const' (params . (opAttr "value" .~ typedNode))
-  where
-    invalidLength = product cShape /= fromIntegral (length values)
-    invalidLengthMsg = printf "invalid tensor length: expected %d got %d"
-                              (product cShape)
-                              (length values)
-    typedNode :: TensorProto
-    typedNode = def
-                & dtype .~ tensorType (undefined :: a)
-                & tensorShape.TensorShape.dim .~
-                      [def & TensorShape.size .~ x | x <- cShape]
-                & tensorVal .~ values
-
--- | Reshape a N-D tensor down to a scalar.
---
--- See `TensorFlow.GenOps.Core.reshape`.
-scalarize :: TensorType a => Tensor v a -> Tensor Build a
-scalarize t = CoreOps.reshape t (vector scalarShape)
-    where
-        scalarShape = [] :: [Int32]
-
--- | Sum a tensor down to a scalar
--- Seee `TensorFlow.GenOps.Core.sum`
-reduceSum :: (OneOf '[ Double, Float, Int32, Int64
-                     , Complex Float, Complex Double] a) =>
-             Tensor v a -> Tensor Build a
-reduceSum x = CoreOps.sum x allAxes
-  where allAxes = CoreOps.range 0 (CoreOps.rank x :: Tensor Build Int32) 1
-
-reduceSum' :: (OneOf '[ Double, Float, Int32, Int64
-                      , Complex Float, Complex Double] a) =>
-              OpParams -> Tensor v a -> Tensor Build a
-reduceSum' params x = CoreOps.sum' params x allAxes
-  where allAxes = CoreOps.range 0 (CoreOps.rank x :: Tensor Build Int32) 1
-
--- | Computes the mean of elements across dimensions of a tensor.
--- See `TensorFlow.GenOps.Core.mean`
-reduceMean
-  :: ( TensorType a
-     , OneOf '[ Double, Float, Complex Float, Complex Double] a
-     )
-  => Tensor v a -> Tensor Build a
-reduceMean = reduceMean' id
-
-reduceMean'
-  :: ( TensorType a
-     , OneOf '[ Double, Float, Complex Float, Complex Double] a
-     )
-  => OpParams -> Tensor v a -> Tensor Build a
-reduceMean' params x = CoreOps.mean' params x allAxes
-  where allAxes = CoreOps.range 0 (CoreOps.rank x :: Tensor Build Int32) 1
-
--- | Create a constant vector.
-vector :: TensorType a => [a] -> Tensor Build a
-vector = vector' id
-
-vector' :: TensorType a => OpParams -> [a] -> Tensor Build a
-vector' params xs = constant' params [fromIntegral $ length xs] xs
-
--- | Create a constant scalar.
-scalar :: TensorType a => a -> Tensor Build a
-scalar = scalar' id
-
-scalar' :: TensorType a => OpParams -> a -> Tensor Build a
-scalar' params x = constant' params [] [x]
-
--- | Random tensor from the unit normal distribution with bounded values.
---
--- This is a type-restricted version of 'TensorFlow.GenOps.Core.truncatedNormal'.
-truncatedNormal :: (MonadBuild m, OneOf '[Word16, Double, Float] a)
-                => Tensor v Int64  -- ^ Shape.
-                -> m (Tensor Value a)
-truncatedNormal = CoreOps.truncatedNormal
-
-truncatedNormal' :: (MonadBuild m, OneOf '[Word16, Double, Float] a)
-                => OpParams -> Tensor v Int64  -- ^ Shape.
-                -> m (Tensor Value a)
-truncatedNormal' = CoreOps.truncatedNormal'
-
-zeros :: forall a . (Num a, TensorType a) => Shape -> Tensor Build a
-zeros (Shape s) = CoreOps.fill (vector s) (scalar 0)
-
-shape :: TensorType t => Tensor v t -> Tensor Build Int32
-shape = CoreOps.shape
-
-shape' :: TensorType t => OpParams -> Tensor v t -> Tensor Build Int32
-shape' = CoreOps.shape'
-
-expandDims :: TensorType t => Tensor v1 t -> Tensor v2 Int32 -> Tensor Build t
-expandDims = CoreOps.expandDims
-
-expandDims' :: TensorType t => OpParams -> Tensor v1 t -> Tensor v2 Int32 -> Tensor Build t
-expandDims' = CoreOps.expandDims'
-
--- | Helper function for reduction ops (translation of math_ops.reduced_shape).
-reducedShape :: (OneOf '[ Int32, Int64 ] t1, OneOf '[ Int32, Int64 ] t2) =>
-                Tensor v1 t1 -> Tensor v2 t2 -> Tensor Build Int32
-reducedShape inputShape axes =
-    let inputShape32 = toInt32 inputShape         -- [2, 3, 5, 7]
-        axes32 = toInt32 axes                     -- [1, 2]
-        toInt32 x = CoreOps.cast x :: Tensor Build Int32
-        inputRank = CoreOps.size inputShape32     -- 4
-        axesMod = (axes32 + inputRank) `CoreOps.mod` inputRank
-        axesShape = shape axesMod                 -- [2]
-    in CoreOps.dynamicStitch                      -- [2, 1, 1, 7]
-         [CoreOps.range 0 inputRank 1,            -- [0, 1, 2, 3]
-           axesMod]                               -- [1, 2]
-         [inputShape32,                           -- [2, 3, 5, 7]
-           CoreOps.fill axesShape 1]              -- [1, 1]
+  print person
